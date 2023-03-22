353f69966abc9311430a8ef6a59bf54bcb107d34
Stanislav Lomakov <stas.lomakov@gmail.com>
2023-03-22 17:32:58 +0200

    Initial Commit

---
+++ Reverse words/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reverse words
+//
+//  Created by Stanislav Lomakov on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
