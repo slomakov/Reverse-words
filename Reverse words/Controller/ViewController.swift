52ef8cddd59c103739be3876af1073f91446c2c7
Stanislav Lomakov <stas.lomakov@gmail.com>
2023-03-24 15:17:50 +0200

    add reverse words functionality

--- Reverse words/Controller/ViewController.swift
+++ Reverse words/Controller/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let rm = ReverseModel()
 
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var textToReverse: UITextField!
+    @IBOutlet weak var reversedText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        isTextFieldEmpty()
     }
 
 
+    @IBAction func reverseButtonPressed(_ sender: UIButton) {
+        if let titleLabel = sender.titleLabel {
+            if titleLabel.text == "Reverse" {
+                reversedText.text = rm.reverseString(textToReverse.text!)
+                sender.setTitle("Clear", for: .normal)
+            } else if titleLabel.text == "Clear" {
+                textToReverse.text?.removeAll()
+                reversedText.text?.removeAll()
+                isTextFieldEmpty()
+                sender.setTitle("Reverse", for: .normal)
+            }
+        }
+    }
+    
+    func isTextFieldEmpty() {
+        reverseButton.isEnabled = false
+        textToReverse.addTarget(self, action: #selector(textFieldIsNotEmpty), for: .editingChanged)
+       }
+    
+    @objc func textFieldIsNotEmpty(sender: UITextField) {
+
+        sender.text = sender.text?.trimmingCharacters(in: .whitespaces)
+        guard
+          let text = textToReverse.text, !text.isEmpty
+          
+          else
+        {
+          self.reverseButton.isEnabled = false
+          return
+        }
+        reverseButton.isEnabled = true
+       }
 }
 
---
+++ Reverse words/Model/ReverseModel.swift
@@ -0,0 +1,19 @@
+//
+//  ReverseModel.swift
+//  Reverse words
+//
+//  Created by Stanislav Lomakov on 22.03.2023.
+//
+
+import Foundation
+
+struct ReverseModel {
+    
+    func reverseString(_ words: String) -> String {
+        let result = words.components(separatedBy: " ")
+            .map { $0.reversed() }
+            .joined(separator: " ")
+        
+        return String(result)
+    }
+}
