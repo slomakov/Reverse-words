c2e1e21458525e2e0cca94ac79427e96d6e95b12
Stanislav Lomakov <stas.lomakov@gmail.com>
2023-04-18 16:42:00 +0300

    Change design and fix linter

--- Reverse words/Controller/ViewController.swift
+++ Reverse words/Controller/ViewController.swift
@@ -13,23 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var reverseButton: UIButton!
     @IBOutlet weak var textToReverseTextField: UITextField!
     @IBOutlet weak var reversedTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         textToReverseTextField.addTarget(self, action: #selector(disableReverseButtonIfNoText), for: .editingChanged)
+        textToReverseTextField.addBottomBorder()
     }
+
     @IBAction func reverseButtonPressed(_ sender: UIButtonWithState) {
         if !sender.isReversed {
             reversedTextLabel.text = reverseModel.reverseString(textToReverseTextField.text!)
             sender.setTitle("Clear", for: .normal)
             sender.isReversed = true
+            textToReverseTextField.endEditing(true)
+            textToReverseTextField.addBottomBorder(color: UIColor.gray)
         } else {
             textToReverseTextField.text?.removeAll()
             reversedTextLabel.text?.removeAll()
             reverseButton.isEnabled = false
             sender.setTitle("Reverse", for: .normal)
+            textToReverseTextField.addBottomBorder(color: UIColor.gray)
             sender.isReversed = false
         }
     }
+
+    @IBAction func editingDidBegan(_ sender: UITextField) {
+        textToReverseTextField.addBottomBorder(color: UIColor.blue)
+    }
+
     @objc func disableReverseButtonIfNoText(sender: UITextField) {
         sender.text = sender.text
         guard
@@ -40,5 +51,15 @@ class ViewController: UIViewController {
             return
         }
         reverseButton.isEnabled = true
+    }
+}
+
+extension UITextField {
+    func addBottomBorder(color: UIColor = UIColor.gray) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 3)
+        bottomLine.backgroundColor = color.cgColor
+        borderStyle = .none
+        layer.addSublayer(bottomLine)
     }
 }
