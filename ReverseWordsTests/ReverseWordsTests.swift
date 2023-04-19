1b8c658012bd0d68ef08d71ddcf35976017cf397
Stanislav Lomakov <stas.lomakov@gmail.com>
2023-04-19 13:05:09 +0300

    Add unit test

---
+++ ReverseWordsTests/ReverseWordsTests.swift
@@ -0,0 +1,35 @@
+//
+//  ReverseWordsTests.swift
+//  ReverseWordsTests
+//
+//  Created by Stanislav Lomakov on 19.04.2023.
+//
+
+import XCTest
+@testable import Reverse_words
+
+final class ReverseWordsTests: XCTestCase {
+    let reverseModel = ReverseModel()
+
+    func testReverseWords() {
+        let result = reverseModel.reverseString("qwer")
+        let expectedResult = "rewq"
+
+        let cases = [
+            ("qwer", "rewq"),
+            ("QWER", "REWQ"),
+            ("qw er", "wq re"),
+            ("qw  er", "wq  re"),
+            ("12345", "54321"),
+            ("!@#$", "$#@!"),
+            ("Qwe123!@#  123", "#@!321ewQ  321")
+        ]
+
+        cases.forEach({ testData, expectedResult in
+            let result = reverseModel.reverseString(testData)
+
+            XCTAssertEqual(result, expectedResult,
+                           "Values did not math: Expected - \(expectedResult), but found \(result) ")
+        })
+    }
+}
